0c56e40ae66bbb43f675ea12c6b282f4c811562b
DenBell <xaldey@mail.ru>
2019-10-18 12:26:26 +0300

    Добавил форму логина и класс пользователя

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 
+from webapp.forms import LoginForm
 from webapp.model import db, News
 
 from webapp.weather import weather_by_city
@@ -16,4 +17,12 @@ def create_app():
         weather = weather_by_city(app.config['WEATHER_DEFAULT_CITY'])
         news_list = News.query.order_by(News.published.desc()).all()
         return render_template('index.html', page_title=title, weather=weather, news_list=news_list)
+
+    @app.route('/login')
+    def login():
+        title = "Авторизация"
+        login_form = LoginForm()
+        return render_template('login.html', page_title=title, form=login_form)
+
+
     return app
--- webapp/model.py
+++ webapp/model.py
@@ -12,3 +12,12 @@ class News(db.Model):
     def __repr__(self):
         return '<News {} {}>'.format(self.title,  self.url)
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), index=True, unique=True)
+    password = db.Column(db.String(128))
+    role = db.Column(db.String(10), index=True)
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+        
