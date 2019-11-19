78812b7634f2dae376e2b8db167e071305503b31
DenBell <xaldey@mail.ru>
2019-11-19 14:54:58 +0300

    Добавил blueprint и делаю реструктуризацию папок

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,9 +1,12 @@
 from flask import Flask, render_template, flash, redirect, url_for
 from flask_login import LoginManager, login_user, logout_user, current_user, login_required
 
-from webapp.forms import LoginForm
-from webapp.model import db, News, User
+from webapp.model import db, News
+from webapp.user.models import User
+from webapp.user.views import blueprint as user_blueprint
+from webapp.admin.views import blueprint as admin_blueprint
 
+#from webapp.forms import LoginForm
 from webapp.weather import weather_by_city
 
 
@@ -14,7 +17,9 @@ def create_app():
 
     login_manager = LoginManager()
     login_manager.init_app(app)
-    login_manager.login_view = 'login'
+    login_manager.login_view = 'user.login'
+    app.register_blueprint(user_blueprint)
+    app.register_blueprint(admin_blueprint)
 
     @login_manager.user_loader
     def load_user(user_id):
@@ -42,7 +47,7 @@ def create_app():
         if form.validate_on_submit():
             user = User.query.filter(User.username == form.username.data).first()
             if user and user.check_password(form.password.data):
-                login_user(user)
+                login_user(user, remember=form.remember_me.data)
                 flash('Вы успешно вошли на сайт')
                 return redirect(url_for('index'))
 
@@ -54,14 +59,5 @@ def create_app():
         logout_user()
         flash('Вы успешно разлогинились')
         return redirect(url_for('index'))
-
-    @app.route('/admin')
-    @login_required
-    def admin_index():
-        if current_user.is_admin:
-            return 'Привет админ!'
-        else:
-            return 'Ты не админ, чувачок :)'
-
 
     return app
